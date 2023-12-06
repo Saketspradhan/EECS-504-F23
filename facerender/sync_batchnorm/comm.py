9e012cff1284de58f19a1f6d381212d1dfb5c172
Shrey Shah <127419891+Shrey-2303@users.noreply.github.com>
2023-12-06 02:54:47 -0500

    imp commit.py

--- facerender/sync_batchnorm/comm.py
+++ facerender/sync_batchnorm/comm.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-# File   : comm.py
-# Author : Jiayuan Mao
-# Email  : maojiayuan@gmail.com
-# Date   : 27/01/2018
-# 
-# This file is part of Synchronized-BatchNorm-PyTorch.
-# https://github.com/vacancy/Synchronized-BatchNorm-PyTorch
-# Distributed under MIT License.
-
 import queue
 import collections
 import threading
